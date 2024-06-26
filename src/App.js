d1428bfed3918815e98d966d9d4b33416eec6ec4
Ekaterine Mashchenko <ekaterine.mashchenko.1@btu.edu.ge>
2024-06-26 20:04:09 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const ACCESS_KEY = "SFyfJJdcrh-AjbHgy5mC00FifkgIfAmo7-dLBs57LEk";
+  useEffect(() => {
+    const fetchPorschePhotos = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.unsplash.com/search/photos",
+          {
+            params: {
+              query: "porsche",
+              client_id: ACCESS_KEY,
+            },
+          }
+        );
+        setPhotos(response.data.results);
+        console.log(response.data.results);
+      } catch (error) {
+        console.error("Error fetching photos from Unsplash:", error);
+      }
+    };
+
+    fetchPorschePhotos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Porsche Photos</h1>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {photos.map((photo) => (
+          <div key={photo.id} style={{ margin: "10px" }}>
+            <img
+              src={photo.urls.small}
+              alt={photo.alt_description}
+              style={{ width: "300px" }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
