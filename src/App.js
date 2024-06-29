8efb3b19f1714a019f9ecc6fb8a972387f1e4c88
“Katherinam4” <ekaterine.mashchenko.1@btu.edu.ge>
2024-06-29 08:45:59 +0400

    set up react-router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  const [photos, setPhotos] = useState([]);
-  const ACCESS_KEY = "SFyfJJdcrh-AjbHgy5mC00FifkgIfAmo7-dLBs57LEk";
-  useEffect(() => {
-    const fetchPorschePhotos = async () => {
-      try {
-        const response = await axios.get(
-          "https://api.unsplash.com/search/photos",
-          {
-            params: {
-              query: "porsche",
-              client_id: ACCESS_KEY,
-            },
-          }
-        );
-        setPhotos(response.data.results);
-        console.log(response.data.results);
-      } catch (error) {
-        console.error("Error fetching photos from Unsplash:", error);
-      }
-    };
-
-    fetchPorschePhotos();
-  }, []);
-
   return (
     <div>
-      <h1>Porsche Photos</h1>
-      <div style={{ display: "flex", flexWrap: "wrap" }}>
-        {photos.map((photo) => (
-          <div key={photo.id} style={{ margin: "10px" }}>
-            <img
-              src={photo.urls.small}
-              alt={photo.alt_description}
-              style={{ width: "300px" }}
-            />
-          </div>
-        ))}
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
